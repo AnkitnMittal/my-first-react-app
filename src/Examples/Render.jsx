dd769dda52411df42659e39d84de72a1331d63ad
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-03-28 06:43:33 +0000

    complete conditional UI rendering

--- src/Examples/Render.jsx
+++ src/Examples/Render.jsx
@@ -1,16 +1,45 @@
-/*  Array.map() method to reduce code redundancy  */
+import { Fragment } from 'react';
+
+const persons = [
+  { id: crypto.randomUUID(), name: 'John', age: 30 },
+  { id: crypto.randomUUID(), name: 'Jane', age: 25 },
+  { id: crypto.randomUUID(), name: 'Bob', age: 35 },
+];
+
+/*
+Place `key` on the top-level element returned by `.map()`
+  Assigned to a 'Child Component' / <div> or use,
+  <Fragment key={...}> if no wrapper element
+*/
+
+function ListItem({ person }) {
+  return (
+    <li>
+      {person.name} - {person.age}
+    </li>
+  );
+}
+
+/*  Conditionally render UI based on the length of the `persons` array.  */
+function List() {
+  if (!persons.length) {
+    return <div>There are no people to display.</div>;
+  }
+
+  return (
+    <ul>
+      {persons.map((person) => (
+        <ListItem key={person.id} person={person} />
+      ))}
+    </ul>
+  );
+}
 
 function Render() {
-  const animals = ['Lion', 'Cow', 'Dog', 'Cat'];
-
   return (
     <>
       <h1>Render</h1>
-      <ul>
-        {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })}
-      </ul>
+      <List />
     </>
   );
 }
